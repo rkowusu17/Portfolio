6bb5c525e6ede059555ddbfa2b873f9d4f2cef51
Reginald Owusu <reginaldmiles45@gmail.com>
2024-01-07 01:23:34 +0000

    year problem

--- apps.js
+++ apps.js
@@ -1,5 +1,10 @@
-let button = getElementbyId('but') 
+function showMeny() {}
 
-function showMeny() {
-    
+function regards() {
+  alert("Thanks for your response");
 }
+
+const year = document.getElementById("year");
+const thisYear = new Date().getFullYear();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
