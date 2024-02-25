b109c219531ab101161ef18edc7861a3de371e31
Reginald Owusu <reginaldmiles45@gmail.com>
2024-02-25 00:47:21 +0000

    Looks good

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./build/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      //     keyframes: {
+      //       "pulse+": {
+      //         "0%": { opacity: "1" },
+      //         "80%": { opacity: "0.5" },
+      //         "100%": { opacity: "1" },
+      //       },
+      //     },
+      //     animation: {
+      //       "pulse+": "pulse+ 1s ease-in-out infinite",
+      //     },
+      //   },
+      // },
+      plugins: [],
+    },
   },
-  plugins: [],
 };
