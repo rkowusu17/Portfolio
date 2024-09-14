6983ef0a39129c067157a925bb4e971314f11d02
Reginald Owusu <reginaldmiles45@gmail.com>
2024-09-14 09:54:15 +0000

    Js error sorted

--- build/js/apps.js
+++ build/js/apps.js
@@ -1,8 +1,3 @@
-const comment = document.getElementById("Comment");
-const email = document.getElementById("e-mail");
-const submitBtn = document.getElementById("submitbtn");
-const resetBtn = document.getElementById("resetbtn");
-
 let year = document.getElementById("year");
 let thisYear = new Date().getFullYear();
 year.textContent = thisYear;
@@ -10,9 +5,9 @@ year.textContent = thisYear;
 // function regards() {
 //   alert("Thank you");
 // }
-// let mobileBtn = document.getElementById("mobile-btn");
-// let closeBtn = document.getElementById("close-btn");
-// let mobileMenu = document.getElementById("mobile-menu");
+let mobileBtn = document.getElementById("mobile-btn");
+let closeBtn = document.getElementById("close-btn");
+let mobileMenu = document.getElementById("mobile-menu");
 
 const initApp = () => {
   const hamburgerBtn = document.getElementById("mobile-btn");
@@ -26,27 +21,6 @@ const initApp = () => {
   mobileMenu.addEventListener("click", toggleMenu);
 };
 
-submitBtn.addEventListener("click", () => {
-  let mail = email.value;
-  let text = comment.value;
-  if (mail.length > 0) {
-    if (text.length <= 1) {
-      alert("Kindly leave a comment ");
-    } else {
-      alert("Thank you for your comment");
-    }
-    console.log(mail);
-    console.log(text.length);
-  }
-  setTimeout(clear(), 5000);
-});
-
-resetBtn.addEventListener("click", clear());
-
-function clear() {
-  email.value = " ";
-  comment.value = " ";
-}
 // console.log(text);
 // console.log(email);
 
