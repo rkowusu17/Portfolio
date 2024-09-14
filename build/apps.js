c15737a5736cc2bb560953286da137d24a20a8b9
Reginald Owusu <reginaldmiles45@gmail.com>
2024-09-14 01:45:31 +0000

    Changed interface & form validation

--- build/apps.js
+++ build/apps.js
@@ -1,10 +1,15 @@
+const comment = document.getElementById("Comment");
+const email = document.getElementById("e-mail");
+const submitBtn = document.getElementById("submitbtn");
+const resetBtn = document.getElementById("resetbtn");
+
 let year = document.getElementById("year");
 let thisYear = new Date().getFullYear();
 year.textContent = thisYear;
 
-function regards() {
-  alert("Thank you");
-}
+// function regards() {
+//   alert("Thank you");
+// }
 let mobileBtn = document.getElementById("mobile-btn");
 let closeBtn = document.getElementById("close-btn");
 let mobileMenu = document.getElementById("mobile-menu");
@@ -20,6 +25,30 @@ const initApp = () => {
   hamburgerBtn.addEventListener("click", toggleMenu);
   mobileMenu.addEventListener("click", toggleMenu);
 };
+
+submitBtn.addEventListener("click", () => {
+  let mail = email.value;
+  let text = comment.value;
+  if (mail.length > 0) {
+    if (text.length <= 1) {
+      alert("Kindly leave a comment ");
+    } else {
+      alert("Thank you for your comment");
+    }
+    console.log(mail);
+    console.log(text.length);
+  }
+  setTimeout(clear(), 5000);
+});
+
+resetBtn.addEventListener("click", clear());
+
+function clear() {
+  email.value = " ";
+  comment.value = " ";
+}
+// console.log(text);
+// console.log(email);
 
 document.addEventListener("DOMContentLoaded", initApp);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/**/*.{html,js}"],
+  content: ["./build/*.{html,js}"],
   theme: {
     extend: {
       //     keyframes: {
